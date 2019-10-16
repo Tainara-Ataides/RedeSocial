6b543e8c9e1ae6412d597ac5a8df237281db0ecf
tainara <tainara@tainara>
2019-10-16 08:59:41 -0300

    conexao do botao alterar cadastro, com tela de cadastro

--- src/redeSocialMTP/TelaPrincipal.java
+++ src/redeSocialMTP/TelaPrincipal.java
@@ -146,6 +146,9 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        new Cadastro().setVisible(true);
+        dispose ();
+        
         new Cadastro(this.u).setVisible(true);
 
     }//GEN-LAST:event_jButton1ActionPerformed
